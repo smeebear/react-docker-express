9a1e44272fcde8b05ea320a711fd695784005b8f
SmeeBear <soheron@gmail.com>
2021-10-08 21:53:58 -0500

    Initializing SSH

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("This is from express")
+});
+
+app.listen(4200, () => {
+    console.log("Server started on port 4200");
+})
